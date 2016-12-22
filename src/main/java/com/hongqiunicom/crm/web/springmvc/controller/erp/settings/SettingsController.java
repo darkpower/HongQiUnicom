05e7552d825b90d71cfbf00e951a2b3cf9689eea
hongqiunicom <darkpower.on@gmail.com>
2016-12-22 19:08:49 +0800

    2016-12-8

--- src/main/java/com/hongqiunicom/crm/web/springmvc/controller/erp/settings/SettingsController.java
+++ src/main/java/com/hongqiunicom/crm/web/springmvc/controller/erp/settings/SettingsController.java
@@ -1,11 +1,8 @@
 package com.hongqiunicom.crm.web.springmvc.controller.erp.settings;
 
-import com.hongqiunicom.crm.services.BroadbandService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.annotation.Resource;
 
 /**
  * Created by Darkpower on 2016/12/10.
@@ -16,10 +13,6 @@ import javax.annotation.Resource;
 @Controller
 @RequestMapping("/ERP/Settings")
 public class SettingsController {
-
-    @Resource
-    private BroadbandService broadbandService;
-
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
